d7681196b4c048a8bba0340bf7ab86dd18d348c1
Bashar Alwarad <beelwarad52@gmail.com>
2024-09-13 16:05:23 +0200

    task4

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,41 @@ const studentData = {
   course: 'Web Development',
   city: 'Berlin',
   picture: 'https://randomuser.me/api/portraits/men/1.jpg',
+  gpa: 100,
+  graduate: false,
 };
 
 // Your components go here
+const Grade = () => {
+  const gpa = studentData.gpa;
+  if (gpa >= 97) {
+    return 'A+';
+  } else if (gpa >= 93) {
+    return 'A';
+  } else if (gpa >= 90) {
+    return 'A-';
+  } else if (gpa >= 87) {
+    return 'B+';
+  } else if (gpa >= 83) {
+    return 'B';
+  } else if (gpa >= 80) {
+    return 'B-';
+  } else if (gpa >= 77) {
+    return 'C+';
+  } else if (gpa >= 73) {
+    return 'C';
+  } else if (gpa >= 70) {
+    return 'C-';
+  } else if (gpa >= 67) {
+    return 'D+';
+  } else if (gpa >= 63) {
+    return 'D';
+  } else if (gpa >= 60) {
+    return 'D-';
+  } else {
+    return 'F';
+  }
+};
 
 const Student = () => {
   return (
@@ -29,6 +61,12 @@ const Student = () => {
       </p>
       <p>
         <strong>City:</strong> {studentData.city}
+      </p>
+      <p>
+        <strong>Grade:</strong> <Grade />
+      </p>
+      <p>
+        <strong>Status:</strong> {studentData.graduate ? 'Graduate' : 'Student'}
       </p>
     </div>
   );
