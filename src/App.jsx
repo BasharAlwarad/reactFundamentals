6a89c17de82ac7f768451d41c6608d6d61532e43
Bashar Alwarad <beelwarad52@gmail.com>
2024-10-04 07:15:19 +0200

    102

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,18 @@
-import { useState } from 'react';
 import DeButton from './component/DeButton';
 import InButton from './component/InButton';
 import OutCome from './component/OutCome';
 import User from './component/User';
 
 import { UserProvider } from './context/userContext';
-import { TodosProvider } from './context/todosContext';
-// import { UserProvider, useUser } from './context/userContext';
 
 function App() {
-  // const { count, setCount } = useUser();
-  const [count, setCount] = useState(1);
-
-  const add = () => setCount(count + 1);
-  const sub = () => setCount(count - 1);
-
   return (
     <div>
       <UserProvider>
-        <TodosProvider>
-          <OutCome count={count} />
-          <InButton onClick={add} />
-          <DeButton onClick={sub} />
-          <User />
-        </TodosProvider>
+        <OutCome />
+        <InButton />
+        <DeButton />
+        <User />
       </UserProvider>
     </div>
   );
