8e94ad919b9a7dfc370427dfddb9f6880b16b553
Bashar Alwarad <beelwarad52@gmail.com>
2024-10-07 08:47:52 +0200

    prop-types and import @ configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
   plugins: [react()],
 });
